a5f226a539b1163ff00c7f298731b81b8aa104ce
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-07-31 18:04:03 -0400

    Add sample data to use for development purposes

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import sampDat from '../../utilities/sample-data.json'
+
+const App = () => {
+  console.log(sampDat)
+  return (
+    <div>
+      <h1>We working</h1>
+    </div>
+  )
+}
+
+export default App
